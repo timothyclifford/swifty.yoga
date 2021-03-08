34b931282d07445b50a2cd063579d844b4b02d93
Timothy Clifford <tim.cliford@gmail.com>
2021-03-08 14:20:12 +1100

    page description

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,20 +5,21 @@ export default function Layout({
 }: {
   children: React.ReactNode;
 }) {
+  const description = "Melbourne / Berlin based Yogi. Vegan foodie & coffee hunter. Dog lover."
   return (
     <div id="fullsingle" className="page-template-page-fullsingle-split body">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="5QIa0Qhd9IEI3orOHks9oDQVquchqFreqIJLlEFD51g" />
-        <meta name="description" content="Berlin based passionate Yogi. Vegan foodie &amp; coffee hunter. Dog lover." />
+        <meta name="description" content={description} />
         <meta name="robots" content="index, follow" />
         <meta name="referrer" content="always" />
         <meta property="og:title" content="Swifty Yoga" />
-        <meta property="og:description" content="Berlin based passionate Yogi. Vegan foodie &amp; coffee hunter. Dog lover." />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="http://swifty.yoga/social.jpg" />
         <meta property="og:url" content="http://swifty.yoga" />
         <meta name="twitter:title" content="Swifty Yoga" />
-        <meta name="twitter:description" content="Passionate Berlin based Yogi. Vegan foodie &amp; coffee hunter. Dog lover." />
+        <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content="http://swifty.yoga/social.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@swifty_yoga" />
@@ -29,7 +30,6 @@ export default function Layout({
 	      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143058888-1"></script>
       </Head>
       {children}
-      <script async src="https://cdn.jsdelivr.net/npm/contentful@latest/dist/contentful.browser.min.js"></script>
     </div>
   );
 }
